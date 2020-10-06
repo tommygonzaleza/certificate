4f3d774b7e35f9dfd8ca2838025b37b0ecb0c123
Omonroy36 <monroyorlando.36@gmail.com>
2020-10-06 23:14:51 +0000

    test

---
+++ pages/api/pdf/[token].js
@@ -0,0 +1,37 @@
+import PDFLayout from "../../../components/PDFLayout";
+import ModernCertificate from "../../../components/certificates/modern";
+import DefaultCertificate from "../../../components/certificates/default";
+import pdfHelper from "../../../auth/pdfHelper";
+import strings from "../../../auth/strings";
+
+export default async (req, res) => {
+      const {
+                query: { token,lang,style },
+            } = req
+    console.log(token)
+    const response = await fetch(`${process.env.BC_HOST}/${token}`);
+    const data = await response.json();
+    if (token !== "" && !data.status_code) {
+        const buffer = await pdfHelper.componentToPDFBuffer(
+            <PDFLayout lang={lang} token={token}>
+                {style === "modern" ? <ModernCertificate data={{
+                    ...data,
+                    token: token,
+                    lang: lang || "en",
+                    strings: strings[lang || "en"]
+                }}
+                /> : <DefaultCertificate data={{
+                    ...data,
+                    token: token,
+                    lang: lang || "en",
+                    strings: strings[lang || "en"]
+                }}
+                    />}
+            </PDFLayout>
+        );
+        // with this header,the browser will open the pdf directly      
+        res.setHeader('Content-Type', 'application/pdf');
+        // output the pdf buffer. once res.end is triggered, it won't trigger the render method
+        res.end(buffer);
+    }
+}
--- pages/api/preview.js
+++
@@ -1,7 +0,0 @@
-export default (req, res) => {
-    if(req.query.token !== ""){
-        return res.status(401).json({ message: 'Invalid token' });
-    }
-    res.setPreviewData({});
-    res.end("Test");
-}
