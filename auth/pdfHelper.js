bb432abf734d263a33064f9cb0a1b20d6034856f
Omonroy36 <monroyorlando.36@gmail.com>
2020-10-10 20:39:51 +0000

    rebase

--- auth/pdfHelper.js
+++ auth/pdfHelper.js
@@ -1,40 +1,37 @@
-/* import { renderToStaticMarkup } from 'react-dom/server';
-import phantomjs from 'phantomjs';
+import { renderToStaticMarkup } from 'react-dom/server';
 import pdf from 'html-pdf';
+import path from "path";
 
+const DIR = process.cwd()
 const componentToPDFBuffer = (component) => {
-  return new Promise((resolve, reject) => {
-    const html = renderToStaticMarkup(component);
-
-    const options = {
-// did not work ->        phantomPath: phantomjs.path,
-// did not work ->       phantomPath: './node_modules/phantomjs/bin/phantomjs',
-        phantomPath: '/vercel/workpath0/node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs',
-        format: 'A4',
-        orientation: 'landscape',
-        type: 'pdf',
-        timeout: 30000,
-    };
-
-    const _buffer = pdf.create(html, options).toBuffer((err, buffer) => {
-        if (err) {
-            console.log("buffer", buffer);
-            console.log("phantomjs.path", phantomjs.path);
-            console.log("Error creating PDF", err);
-            return reject(err);
-        }
-    
-        return resolve(buffer);
+    return new Promise((resolve, reject) => {
+        const html = renderToStaticMarkup(component);
+        const options = {
+            format: 'A4',
+            orientation: 'landscape',
+            type: 'pdf',
+            timeout: 30000,
+            phantomPath: path.resolve(
+                process.cwd(),
+                "node_modules/phantomjs-prebuilt/lib/phantom/bin/phantomjs"
+            )
+        };
+        console.log(html)
+        process.env.FONTCONFIG_PATH = path.join(DIR, "fonts");
+        process.env.LD_LIBRARY_PATH = path.join(DIR, "bins");
+        const result = pdf.create(html, options);
+        result.toBuffer((err, buffer) => {
+            if (err) {
+                console.log("ERROR Genering PDF: ", err)
+                return reject(err);
+            }
+            
+            console.log("SUCCESS Genering PDF")
+            return resolve(buffer);
+        });
     });
-  });
-  const page = await browser.newPage();
-  await page.setContent(html, { waitUntil: 'networkidle0', timeout: 30000 });
-  const pdf = await page.pdf({
-    format: 'A4',
-  });
-await browser.close();
-  return pdf;
-};
+}
+
 export default {
-  componentToPDFBuffer
-}; */
+    componentToPDFBuffer
+}
--- components/PDFLayout.js
+++ components/PDFLayout.js
@@ -1,21 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const PDFLayout = ({ children,lang,token }) => (
-  <html>
-    <head>
-      <meta charSet="utf8" />
-      <link href="https://fonts.googleapis.com/css2?family=Lato:wght@300;400;700;900&display=swap" rel="stylesheet" />
-      <link href="https://fonts.googleapis.com/css2?family=Mrs+Saint+Delafield&display=swap" rel="stylesheet"></link>
-    </head>
-    <body style={{margin: 0}}>
-      {children} 
-    </body>
-  </html>
+
+const PDFLayout = ({ children, lang, token }) => (
+        <html>
+            <head>
+                <meta charSet="utf8" />
+            </head>
+            <body style={{ margin: 0}}>
+                {children}
+            </body>
+        </html>
 );
 
 PDFLayout.propTypes = {
-  children: PropTypes.node,
+    children: PropTypes.node,
 };
 
 export default PDFLayout;
